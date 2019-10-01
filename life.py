1d70c560de42072ed7287bde4bc2c7a984599134
Rishabh Choudhary <38792773+RisavChd24@users.noreply.github.com>
2019-10-01 22:34:02 +0530

    Create life.py

---
+++ life.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 22:27:10 2019
+
+@author: risavchd24
+"""
+
+while 1 :
+     a = int(input())
+     if a == 42 :
+         break
+     else :
+         print(a)
