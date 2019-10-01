51f4b7e926dc4555395c47f3770cad3a874aa9da
Abraham Francis <abfr049@gmail.com>
2019-10-01 22:11:05 +0530

    Solve Life, the Universe and Everything

---
+++ TEST.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	short N;
+	while (true) {
+		cin >> N;
+		if (N == 42) break;
+		cout << N << '\n';
+	}
+}
