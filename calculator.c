ad78dcacf8c387e552f4cf3034e57383199cd934
Maia <48120550+Maia21@users.noreply.github.com>
2019-10-02 14:01:22 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,3 +1,20 @@
-int calculator(int x, int y){
+void calculator(int x, int y, char op){
+    switch(op)
+    {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf", x, y, x + y);
+            break;
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf", x, y, x - y);
+            break;
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf", x, y, x * y);
+            break;
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf", x, y, x / y);
+            break;
 
+        default:
+            printf("Error! operator is not correct");
+    }
 }
