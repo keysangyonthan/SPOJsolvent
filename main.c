fdfafb8d909fffc7eea3b557aaafdd52d46394cb
Maia <48120550+Maia21@users.noreply.github.com>
2019-10-02 14:01:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 
 int main() {
     int x, y;
+    char op;
     printf("Enter the 1st number");
     scanf("&d", x);
     printf("Enter the 2st number");
     scanf("&d", y);
-    calculator(x,y);
+    printf("Enter the operator")
+    scanf("%c", op);
+    calculator(x,y,op);
 
     return 0;
 }
