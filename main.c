cc9fb658da939de59635891b9b5b47d491b44c5f
Maia <48120550+Maia21@users.noreply.github.com>
2019-10-02 13:55:13 +0100

    Main created
    Implementation of the main class

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("Enter the 1st number");
+    scanf("&d", x);
+    printf("Enter the 2st number");
+    scanf("&d", y);
+    calculator(x,y);
+
+    return 0;
+}
